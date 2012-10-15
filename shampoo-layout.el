9423bb06093d6c1bbd9f315f39669aaaed7193db
Dmitry Matveev <me@dmitrymatveev.co.uk>
2012-10-15 18:19:41 +0300

    Main window layout improvements
    - dropped the old ugly macro, introduced pretty combinators
    - added interactive shampoo-restore-layout function

--- shampoo-layout.el
+++ shampoo-layout.el
@@ -5,35 +5,79 @@
 ;; This software is released under terms of the MIT license,
 ;; please refer to the LICENSE file for details.
 
+(eval-when-compile (require 'cl))
 (require 'shampoo-modes)
 
-(defmacro shampoo-generate-layout-impl (surface &rest data-list)
-  (let ((decls '()) (commands '()) (buffa (make-symbol "buffa")))
-    (dolist (data data-list)
-      (destructuring-bind (sym wnd bfr code mode) data
-        (pushnew `(let ((,buffa (get-buffer-create ,bfr)))
-                    (set-window-buffer ,wnd ,buffa)
-                    (save-excursion
-                      (set-buffer ,buffa)
-                      (,mode))
-                    (puthash ',sym ,wnd surface))
-                 commands)
-        (pushnew `(,wnd ,code) decls)))
-    `(let* (,@decls) ,@commands)))
+(defun shampoo-generic-splitter (args split-fcn)
+  (lexical-let ((row-funcs args)
+                (splitter split-fcn))
+    (lambda (wnd)
+      (let ((head (first row-funcs))
+            (tail (rest row-funcs)))
+        (dolist (f (reverse tail))
+          (funcall f (funcall splitter wnd)))
+        (funcall head wnd)))))
 
-;; TODO refactor this
-(defun shampoo-create-layout ()
+(defun shampoo-rows (&rest args)
+  (shampoo-generic-splitter args 'split-window))
+
+(defun shampoo-cols (&rest args)
+  (shampoo-generic-splitter args (lambda (w) (split-window w nil t))))
+
+(defun shampoo-build-layout (layout-desc)
   (delete-other-windows)
-  (let ((root (selected-window))
-        (surface (make-hash-table)))
-    (shampoo-generate-layout-impl
-     surface
-     (nspc-w nspc "*shampoo-namespaces*" root shampoo-namespaces-list-mode)
-     (mets-w meth "*shampoo-methods*"    (split-window cats nil t) shampoo-methods-list-mode)
-     (clss-w clss "*shampoo-classes*"    (split-window root nil t) shampoo-classes-list-mode)
-     (cats-w cats "*shampoo-categories*" (split-window root nil t) shampoo-cats-list-mode)
-     (code-w code "*shampoo-code*"       (split-window root) shampoo-code-mode))
-    surface))
+  (funcall layout-desc (selected-window))
+  (balance-windows))
+
+(defun* shampoo-make-window-setup (&key buffer-name mode-to-use)
+  (lexical-let ((buffer buffer-name)
+                (mode mode-to-use))
+    (lambda (wnd)
+      (let ((buff (get-buffer-create buffer)))
+        (set-window-buffer wnd buff)
+        (save-excursion
+          (set-buffer buff)
+          (funcall mode))))))
+
+(defun shampoo-layout-desc ()
+  (shampoo-rows
+   (shampoo-cols (shampoo-setup-namespaces-window)
+                 (shampoo-setup-classes-window)
+                 (shampoo-setup-categories-window)
+                 (shampoo-setup-methods-window))
+   (shampoo-setup-source-window)))
+
+(defun shampoo-setup-namespaces-window ()
+  (shampoo-make-window-setup
+   :buffer-name "*shampoo-namespaces*"
+   :mode-to-use 'shampoo-namespaces-list-mode))
+
+(defun shampoo-setup-classes-window ()
+  (shampoo-make-window-setup
+   :buffer-name "*shampoo-classes*"
+   :mode-to-use 'shampoo-classes-list-mode))
+
+(defun shampoo-setup-categories-window ()
+  (shampoo-make-window-setup
+   :buffer-name "*shampoo-categories*"
+   :mode-to-use 'shampoo-cats-list-mode))
+
+(defun shampoo-setup-methods-window ()
+  (shampoo-make-window-setup
+   :buffer-name "*shampoo-methods*"
+   :mode-to-use 'shampoo-methods-list-mode))
+
+(defun shampoo-setup-source-window ()
+  (shampoo-make-window-setup
+   :buffer-name "*shampoo-code*"
+   :mode-to-use 'shampoo-code-mode))
+
+(defun shampoo-create-layout ()
+  (shampoo-build-layout (shampoo-layout-desc)))
+
+(defun shampoo-restore-layout ()
+  (interactive)
+  (shampoo-create-layout))
 
 (provide 'shampoo-layout)
 
