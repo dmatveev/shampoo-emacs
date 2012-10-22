3c8008212bf0531066b5e421df896057dae2ad64
Felix Lange <fjl@twurst.com>
2012-10-22 23:19:28 +0200

    fix some requires so byte-compilation works

--- shampoo-dict.el
+++ shampoo-dict.el
@@ -5,6 +5,8 @@
 ;; This software is released under terms of the MIT license,
 ;; please refer to the LICENSE file for details.
 
+(require 'cl)
+
 (defun make-shampoo-dict ()
   (make-hash-table))
 
